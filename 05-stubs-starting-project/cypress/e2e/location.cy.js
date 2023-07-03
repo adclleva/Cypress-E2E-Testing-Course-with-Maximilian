4fb66e59cdbdea022d85d7f3a07d185eb8e25138
Arvin Lleva <arvin.lleva@yahoo.com>
2023-07-02 18:21:53 -0700

    added the fake stub implementation to simulate the laoding state

--- 05-stubs-starting-project/cypress/e2e/location.cy.js
+++ 05-stubs-starting-project/cypress/e2e/location.cy.js
@@ -8,11 +8,33 @@ describe('share location', () => {
     // when accessing the / route
     cy.visit('/').then((window) => {
       // we create an alias of this stub
-      cy.stub(window.navigator.geolocation, 'getCurrentPosition').as('getUserPosition')
+      cy.stub(window.navigator.geolocation, 'getCurrentPosition')
+      .as('getUserPosition')
+      // this function replaces getCurrentPosition with a fake function
+      // this get executed immediately when the function is called
+      .callsFake((cb) => { // cb is the callback function
+        // we use a timeout to simulate the delay of the function
+        // to get the loading spinner
+        setTimeout(() => {
+          cb(
+            {
+              coords: {
+                latitude: 37.5,
+                longitude: 48.01,
+              }
+            }
+          )
+        }, 100)
+      })
     });
 
     cy.get('[data-cy="get-loc-btn"]').click();
     // we can "get" an alias and check if it was called
     cy.get('@getUserPosition').should('have.been.called')
+
+    cy.get('[data-cy="get-loc-btn"]').should('be.disabled')
+
+    cy.get('[data-cy="actions"]').should('contain', 'Location fetched!') // alternative to contains()
+
   });
 });
