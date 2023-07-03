14688b59c1f85fd71771431e68fd8bd4b5a266ba
Arvin Lleva <arvin.lleva@yahoo.com>
2023-07-02 18:04:27 -0700

    npm installed

--- 05-stubs-starting-project/cypress/e2e/location.cy.js
+++ 05-stubs-starting-project/cypress/e2e/location.cy.js
@@ -2,7 +2,17 @@
 
 describe('share location', () => {
   it('should fetch the user location', () => {
-    cy.visit('/');
+    // using then helps us get direct access to the element for example
+    // get yields the window object
+    // we do this because we know that we need to get access to the window object
+    // when accessing the / route
+    cy.visit('/').then((window) => {
+      // we create an alias of this stub
+      cy.stub(window.navigator.geolocation, 'getCurrentPosition').as('getUserPosition')
+    });
+
     cy.get('[data-cy="get-loc-btn"]').click();
+    // we can "get" an alias and check if it was called
+    cy.get('@getUserPosition').should('have.been.called')
   });
 });
